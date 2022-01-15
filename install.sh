711d7a8d392703e37c8f8748ae8565961a4da747
Fornball <46828271+Fornball@users.noreply.github.com>
2022-01-15 15:14:48 +0100

    Combined install scripts into one

--- install.sh
+++ install.sh
@@ -1,18 +1,24 @@
-echo "Downloading files"
-mkdir /usr/local/Fanctrl
-cd /usr/local/Fanctrl
-curl "https://raw.githubusercontent.com/Fornball/chromeos-autofanctrl/main/LICENSE"
-curl "https://raw.githubusercontent.com/Fornball/chromeos-autofanctrl/main/README.md"
-curl "https://raw.githubusercontent.com/Fornball/chromeos-autofanctrl/main/fanctrl.sh"
-curl "https://raw.githubusercontent.com/Fornball/chromeos-autofanctrl/main/install.sh"
+if [ "$1" = "-rebooted" ]; then
+  echo "Installing part 2/2."
+  cp /usr/local/Fanctrl/fanctrl-custom.conf /etc/init/
+  echo "Installation is complete."
+else    
+  echo "Installing part 1/2."
+  mkdir /usr/local/Fanctrl
+  cd /usr/local/Fanctrl
+  curl "https://raw.githubusercontent.com/Fornball/chromeos-autofanctrl/main/LICENSE"
+  curl "https://raw.githubusercontent.com/Fornball/chromeos-autofanctrl/main/README.md"
+  curl "https://raw.githubusercontent.com/Fornball/chromeos-autofanctrl/main/fanctrl.sh"
+  
+  echo "Extracting files."
+  crossystem dev_boot_signed_only=0
+  /usr/share/vboot/bin/make_dev_ssd.sh --remove_rootfs_verification
+  echo "Which partition to unlock?"
+  read partition_id
+  /usr/share/vboot/bin/make_dev_ssd.sh --remove_rootfs_verification --partitions $partition_id
+fi
 
-echo "Installing script"
-crossystem dev_boot_signed_only=0
-/usr/share/vboot/bin/make_dev_ssd.sh --remove_rootfs_verification
-echo "Which partition to unlock?"
-read partition_id
-/usr/share/vboot/bin/make_dev_ssd.sh --remove_rootfs_verification --partitions $partition_id
-
-echo "Chromebook will reboot in 5 seconds"
+echo "Chromebook will reboot in 5 seconds."
 sleep 5
-sudo reboot
+reboot
+exit 1
