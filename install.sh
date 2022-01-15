6971a52045b28b78ac2a19fc1c2c5d2710edd2d5
Fornball <46828271+Fornball@users.noreply.github.com>
2022-01-15 21:45:18 +0100

    Removed user prompt for partition id

--- install.sh
+++ install.sh
@@ -9,13 +9,13 @@ else
   curl "https://raw.githubusercontent.com/Fornball/chromeos-autofanctrl/main/LICENSE"
   curl "https://raw.githubusercontent.com/Fornball/chromeos-autofanctrl/main/README.md"
   curl "https://raw.githubusercontent.com/Fornball/chromeos-autofanctrl/main/fanctrl.sh"
+  curl "https://raw.githubusercontent.com/Fornball/chromeos-autofanctrl/main/fanctrl-custom.conf"
   
   echo "Extracting files."
   crossystem dev_boot_signed_only=0
-  /usr/share/vboot/bin/make_dev_ssd.sh --remove_rootfs_verification
-  echo "Which partition to unlock?"
-  read partition_id
-  /usr/share/vboot/bin/make_dev_ssd.sh --remove_rootfs_verification --partitions $partition_id
+  echo "Removing read-write protection"
+  partition_id=$(sudo /usr/share/vboot/bin/make_dev_ssd.sh --remove_rootfs_verification | grep -m1 -o "partitions.*")
+  /usr/share/vboot/bin/make_dev_ssd.sh --remove_rootfs_verification --$partition_id
 fi
 
 echo "Chromebook will reboot in 5 seconds."
