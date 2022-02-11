f35904d74d956c5f739aff1620081c5c1a6fb68c
Fornball <46828271+Fornball@users.noreply.github.com>
2022-02-11 12:30:52 +0100

    Combined fanctrl.sh and fanctrl-custom.conf

--- install.sh
+++ install.sh
@@ -10,17 +10,16 @@ else
   echo "Installing part 1/2."
   mkdir /usr/local/Fanctrl
   cd /usr/local/Fanctrl
+  echo "Downloading files"
   curl "https://raw.githubusercontent.com/Fornball/chromeos-autofanctrl/main/LICENSE"
   curl "https://raw.githubusercontent.com/Fornball/chromeos-autofanctrl/main/README.md"
-  curl "https://raw.githubusercontent.com/Fornball/chromeos-autofanctrl/main/fanctrl.sh"
   curl "https://raw.githubusercontent.com/Fornball/chromeos-autofanctrl/main/fanctrl-custom.conf"
   
-  echo "Extracting files."
-  crossystem dev_boot_signed_only=0
   echo "Removing read-write protection"
+  crossystem dev_boot_signed_only=0
   partition_id=$(sudo /usr/share/vboot/bin/make_dev_ssd.sh --remove_rootfs_verification | grep -m1 -o "partitions.*")
   /usr/share/vboot/bin/make_dev_ssd.sh --remove_rootfs_verification --$partition_id
-  echo "Chromebook will reboot in 5 seconds."
+  echo "Chrome OS will reboot in 5 seconds."
   sleep 5
   reboot
   exit 1
